bc0a520e5aea442099f85ba9b35a653b3322546e
Grace <gracedme.github@gmail.com>
2020-11-23 17:43:41 -0800

    ✨ day 02 part 2 - ribbon

--- 2015/day02/day02.test.js
+++ 2015/day02/day02.test.js
@@ -1,4 +1,5 @@
 const getTotalWrappingPaper = require('./one');
+const getTotalRibbon = require("./two");
 
 describe("part one: getTotalWrappingPaper(input)", () => {
    test("one toybox area", () => {
@@ -9,4 +10,16 @@ describe("part one: getTotalWrappingPaper(input)", () => {
    test("multiple toyboxen", () => {
         expect(getTotalWrappingPaper("2x3x4\n1x1x10")).toBe(101);
    })
+});
+
+describe("part two: getTotalRibbon(input)", () => {
+   test("one toybox ribbon", () => {
+       expect(getTotalRibbon("2x3x4")).toBe(34);
+       expect(getTotalRibbon("1x1x10")).toBe(14);
+       expect(getTotalRibbon("14x12x8")).toBe(1384);
+   });
+
+   test("multiple toyboxen", () => {
+       expect(getTotalRibbon("2x3x4\n1x1x10\n2x3x4\n1x1x10")).toBe(96);
+   });
 });
---
+++ 2015/day02/two.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const filename = process.argv[2];
+
+const stream = fs.createReadStream(filename);
+stream.setEncoding('utf8');
+stream.on("data", getTotalRibbon);
+
+function getTotalRibbon(input) {
+    const toys = input.split('\n');
+    let totalRibbon = 0;
+    let count = 1;
+
+    // for each line, get wrapping paper area
+    toys.forEach(val => {
+        // split line into length, width, height
+        const dimensions = val.split('x');
+
+        // calculate rectangle areas
+        const rectangles = [
+            2 * (parseInt(dimensions[0]) + parseInt(dimensions[1])),
+            2 * (parseInt(dimensions[0]) + parseInt(dimensions[2])),
+            2 * (parseInt(dimensions[1]) + parseInt(dimensions[2]))
+        ].sort((a, b) => a - b);
+        const smallestFace = rectangles[0];
+        console.log(`smallestFace: ${smallestFace}`);
+
+        // calculate surface area
+        const volume = dimensions[0] * dimensions[1] * dimensions[2];
+        console.log(`volume: ${volume}`);
+        const ribbon = smallestFace + volume;
+
+        // add to total
+        totalRibbon += ribbon;
+        console.log(`totalRibbon: ${totalRibbon}`);
+        console.log(count);
+        count++;
+
+    });
+    return totalRibbon;
+}
+
+module.exports = getTotalRibbon;
