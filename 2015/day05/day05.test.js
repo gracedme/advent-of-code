fd867231c622633d46cbaa7340b29c5b50f03694
Grace <gracedme.github@gmail.com>
2020-11-26 12:34:02 -0800

    ✨ day 5 part one - nice strings in list

--- 2015/day05/day05.test.js
+++ 2015/day05/day05.test.js
@@ -1,9 +1,18 @@
 const fn = require('./one');
 
 describe("part one: getTotalNiceStrings(input)", () => {
-   test("examples", () => {
+   test("nice strings", () => {
        expect(fn("ugknbfddgicrmopn\naaa")).toBe(2);
        expect(fn("aaa")).toBe(1);
-       expect(fn("jchzalrnumimnmhp\nhaegwjzuvuyypxyu\ndvszwmarrgswjxmb")).toBe(0);
    });
+
+   test("excludes blacklist", () => {
+       expect(fn("haegwjzuvuyypxyu")).toBe(0);
+   });
+   test("has consecutive duplicate characters", () => {
+       expect(fn("jchzalrnumimnmhp")).toBe(0);
+   });
+   test("has at least three vowels", () => {
+       expect(fn("dvszwmarrgswjxmb")).toBe(0);
+   })
 });
--- 2015/day05/one.js
+++ 2015/day05/one.js
@@ -1,43 +1,30 @@
 const fs = require('fs');
 
-const stream = fs.createReadStream(`input.txt`);
+const stream = fs.createReadStream(__dirname + `/input.txt`);
 stream.setEncoding('utf8');
 stream.on("data", fn);
 
 function fn(input) {
     const lines = input.split('\n');
-    const vowels = "aeiou".split('');
 
     const excludesBlacklist = str =>
         !(str.includes("ab") || str.includes("cd")
             || str.includes("pq") || str.includes("xy"));
 
+    const includesDoubles = str =>
+        str.split('')
+            .filter((ch, ix) => ch === str[ix + 1])
+            .length > 0;
 
-    const excludesDoubles = str => {
-        // get all unique chars from str
-        // that occur more than once
-        let chars = new Set(str);
-        let totals = {};
-
-        const something = chars.forEach(ch => {
-
-        })
-        // search for duplicates
-
-
-
-        return true;
-    }
-
-    const atLeastThreeVowels = str => {
-
-        return true;
-    }
+    const atLeastThreeVowels = str =>
+        str.split('')
+            .filter(ch => ['a', 'e', 'i', 'o', 'u'].includes(ch))
+            .length >= 3;
 
     let totalNice = 0;
     lines.forEach(line => {
         if (excludesBlacklist(line)
-            && excludesDoubles(line)
+            && includesDoubles(line)
             && atLeastThreeVowels(line)
         ) {
             totalNice++;
