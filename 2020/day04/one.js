11afeaab19cf842d2edf15c4050428dd26789b20
Grace <gracedme.github@gmail.com>
2020-12-05 11:57:23 -0800

    🎄⭐ day 04 part 1 - this is terrible but it works, blergh

--- 2020/day04/one.js
+++ 2020/day04/one.js
@@ -1,31 +1,41 @@
 const fs = require('fs');
 
-const stream = fs.createReadStream(__dirname + `/input.txt`);
-stream.setEncoding('utf8');
-stream.on("data", fn);
+const filename = process.argv[2]
+
+if (filename) {
+  const stream = fs.createReadStream(__dirname + `/${filename}`);
+  stream.setEncoding('utf8');
+  stream.on("data", fn);
+}
 
 function fn(input) {
-  const hasAllFields = pp => pp.hasOwnProperty("byr")
-      && pp.hasOwnProperty("iyr")
-      && pp.hasOwnProperty("eyr")
-      && pp.hasOwnProperty("hgt")
-      && pp.hasOwnProperty("hcl")
-      && pp.hasOwnProperty("ecl")
-      && pp.hasOwnProperty("pid");
+  const requiredFields = ["byr", "ecl", "eyr", "hcl", "hgt", "iyr", "pid"];
+  let totalValid = 0;
 
-  const passports = input.split(/\s\s/) // chunk passport groups
-      .map(pp => pp.split(/\s/)) // isolate key:val pairs
-      .map(pp => pp
-        .map(p => p.split(':')) // split into [key, val]
-        .reduce((acc, current) => { // create an obj instead of array
-          if (current[0] && current[1]) acc[current[0]] = current[1];
-          return acc;
-        }, {})
-      )
-    .filter(pp => hasAllFields(pp))
+  const passports = input.split(/\s\s/);
 
-  console.log(passports.length);
-  return passports.length;
+  const isValid = pp => {
+    if (pp.length < requiredFields.length) return false;
+
+    let valid = true;
+    for (let ix = 0; ix < pp.length; ix++) {
+      if (requiredFields.indexOf(pp[ix].substr(0, 3)) === -1 && pp.length === requiredFields.length) {
+        valid = false;
+      }
+    }
+
+    return valid;
+  }
+
+  for (let ix = 0; ix < passports.length; ix++) {
+    const pairs = passports[ix].split(/\s/)
+    if (isValid(pairs)) {
+      totalValid++;
+    }
+  }
+
+  console.log(totalValid);
+  return totalValid;
 }
 
 module.exports = fn;
