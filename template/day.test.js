87adacfaed560c563c5833f1ae3b4e416eec0863
Grace <gracedme.github@gmail.com>
2020-12-06 15:45:37 -0800

    ♻ redo template and initialize day 06

---
+++ template/day.test.js
@@ -0,0 +1,18 @@
+const one = require('./one');
+const two = require('./two');
+
+const actualInput = ``;
+
+describe("day ", () => {
+  describe("part 1", () => {
+    test("actual input", () => {
+      // expect(one(actualInput)).toBe();
+    })
+  });
+
+  describe("part 2", () => {
+    test("actual input", () => {
+      // expect(two(actualInput)).toBe();
+    })
+  })
+});
