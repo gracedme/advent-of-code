4400a1358f77824448c3b4a0e7f561707962c524
Grace <gracedme.github@gmail.com>
2020-12-09 12:48:47 -0800

    🎄⭐ 2020-07-02: total bags in shiny gold bag 🏫 recursion is hard 🏫 consider other data structures; objects have limitations

--- 2020/day07/day.test.js
+++ 2020/day07/day.test.js
@@ -3,8 +3,7 @@ const two = require('./two');
 const ACTUAL_INPUT = require('./input');
 
 describe("day 7", () => {
-  describe("part 1", () => {
-    const testInput = `faded blue bags contain no other bags.
+  const testInputOne = `faded blue bags contain no other bags.
 dim bronze bags contain 5 vibrant plum bags, 3 light red bags, 2 dotted black bags, 1 faded blue bag.
 light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
@@ -14,8 +13,10 @@ shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
 dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 dotted black bags contain no other bags.`;
+  describe("part 1", () => {
+
     test("sample input", () => {
-      expect(one(testInput)).toBe(5);
+      expect(one(testInputOne)).toBe(5);
     })
 
     test("actual input", () => {
@@ -24,9 +25,24 @@ dotted black bags contain no other bags.`;
   });
 
   describe("part 2", () => {
+    const testInputTwo = `shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`;
+
+    test("sample input one", () => {
+      expect(two(testInputOne)).toBe(32);
+    });
+
+    test("sample input two", () => {
+      expect(two(testInputTwo)).toBe(126);
+    })
+
     test("actual input", () => {
-      // expect(two(ACTUAL_INPUT)).toBe
-      // ();
+      expect(two(ACTUAL_INPUT)).toBe(6006);
     })
   })
 });
--- 2020/day07/two.js
+++ 2020/day07/two.js
@@ -10,9 +10,45 @@ if (filename) {
 
 function fn(input) {
   const lines = input.split('\n');
-  let result = 0;
 
-  return result;
+  let luggageMap = {};
+  lines.forEach(line => {
+    const matches = line.match(/(\w+ \w+) bags contain (no other bags|(?:\d+ \w+ \w+ bags?,? ?)+)./);
+    const bag = matches[1];
+    const sources = matches[2].split(', ');
+
+    luggageMap[bag] = sources.reduce((acc, item) =>{
+      if (item === "no other bags") return {};
+      const details = item.split(' ');
+
+      return {
+        ...acc,
+        [`${details[1]} ${details[2]}`]: parseInt(details[0])
+      }
+    }, {});
+  });
+
+  let lookFor = "shiny gold";
+
+  // todo: not quite right, returns correct total + 1
+  // todo: re-think the objects here, should be different data structure
+  function getBagsCount(colour) {
+    const children = luggageMap[colour];
+
+    // if there are no children
+    // return 1
+    if (!Object.keys(children).length) return 1;
+
+    return Object.entries(children)
+      .reduce((acc, [key, val]) =>
+        acc + val * getBagsCount(key), 0)
+      + 1; // include container bag
+  }
+
+  const total = getBagsCount(lookFor) - 1;
+
+  console.log(`total in ${lookFor}`, total);
+  return total;
 }
 
 module.exports = fn;
