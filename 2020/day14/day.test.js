f9c9b98fdcf47e8bb0241790da10ad7b8cbff1dd
Grace <gracedme.github@gmail.com>
2020-12-21 13:26:32 -0800

    🤔 spent some time understanding 13b but don't have time to solve. Prepping day 14

---
+++ 2020/day14/day.test.js
@@ -0,0 +1,27 @@
+const one = require('./one');
+const two = require('./two');
+const ACTUAL_INPUT = require('./input');
+
+
+describe("day ", () => {
+  const testInput = ``;
+  describe("part 1", () => {
+    test("test input", () => {
+      // expect(one(testInput)).toBe();
+    })
+
+    test("actual input", () => {
+      // expect(one(ACTUAL_INPUT)).toBe();
+    })
+  });
+
+  describe("part 2", () => {
+    test("test input", () => {
+      // expect(two(testInput)).toBe();
+    })
+
+    test("actual input", () => {
+      // expect(two(ACTUAL_INPUT)).toBe();
+    })
+  })
+});
