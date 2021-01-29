c909f8120ffface7e9e9297ddffa88855c337ade
Grace <gracedme.github@gmail.com>
2021-01-29 13:12:32 -0800

    🎄⭐ 2020-13-02: earliest bus alignment 🤔🏫 needed hints for this one. did not go the Chinese Remainder Theorem route

--- 2020/day13/day.test.js
+++ 2020/day13/day.test.js
@@ -7,8 +7,7 @@ const TEST_INPUT = `939
 const ACTUAL_INPUT = `1015292
 19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,743,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,643,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23`;
 
-describe("day ", () => {
-  const testInput = ``;
+describe("day 13", () => {
   describe("part 1", () => {
     test("test input", () => {
       expect(one(TEST_INPUT)).toBe(295);
@@ -20,12 +19,26 @@ describe("day ", () => {
   });
 
   describe("part 2", () => {
+    const test_input_a = `\n17,x,13,19`;
+    const test_input_b = `\n67,7,59,61`;
+    const test_input_c = `\n67,x,7,59,61`;
+    const test_input_d = `\n67,7,x,59,61`;
+    const test_input_e = `\n1789,37,47,1889`;
+
     test("test input", () => {
-      // expect(two(testInput)).toBe();
+      expect(two(TEST_INPUT)).toBe(1068781);
+    })
+
+    test("test small examples", () => {
+      expect(two(test_input_a)).toBe(3417);
+      expect(two(test_input_b)).toBe(754018);
+      expect(two(test_input_c)).toBe(779210);
+      expect(two(test_input_d)).toBe(1261476);
+      expect(two(test_input_e)).toBe(1202161486);
     })
 
     test("actual input", () => {
-      // expect(two(ACTUAL_INPUT)).toBe();
+      expect(two(ACTUAL_INPUT)).toBe(1001569619313439);
     })
   })
 });
--- 2020/day13/two.js
+++ 2020/day13/two.js
@@ -9,10 +9,25 @@ if (filename) {
 }
 
 function fn(input) {
-  const lines = input.split('\n');
-  let result = 0;
+  const lines = input.split('\n')
+  const BUSES = lines[1].split(',')
+    .map((id, remainder) => ({ id, remainder }))
+    .filter(o => o.id !== 'x')
+    .map(o => ({ id: parseInt(o.id), remainder: o.remainder }))
 
-  return result;
+  console.log(BUSES);
+
+  let timestamp = BUSES.shift().id;
+  let periodIncrement = timestamp;
+  for (let { id, remainder } of BUSES) {
+      while ((timestamp + remainder) % id !== 0) {
+        timestamp += periodIncrement
+      }
+      periodIncrement *= id
+  }
+
+  console.log(`earliest aligned bus schedule time: ${timestamp}`);
+  return timestamp;
 }
 
 module.exports = fn;
