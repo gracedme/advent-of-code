19e236f02e86b13a22f398387594d2aa689c7029
Grace <gracedme.github@gmail.com>
2021-02-04 14:39:50 -0800

    🎄⭐ 2020-14-01: bitmaskin'

--- 2020/day14/day.test.js
+++ 2020/day14/day.test.js
@@ -3,15 +3,18 @@ const two = require('./two');
 const ACTUAL_INPUT = require('./input');
 
 
-describe("day ", () => {
-  const testInput = ``;
+describe("day 14", () => {
+  const testInput = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0`;
   describe("part 1", () => {
     test("test input", () => {
-      // expect(one(testInput)).toBe();
+      expect(one(testInput)).toBe(165);
     })
 
     test("actual input", () => {
-      // expect(one(ACTUAL_INPUT)).toBe();
+      expect(one(ACTUAL_INPUT)).toBe(7817357407588);
     })
   });
 
--- 2020/day14/one.js
+++ 2020/day14/one.js
@@ -10,9 +10,42 @@ if (filename) {
 
 function fn(input) {
   const lines = input.split('\n');
-  let result = 0;
+  let checksum = 0;
 
-  return result;
+  let bitmask = ``;
+  let memory = {};
+  const bitMaskRegex = /mask = ([01X]+)/;
+  const memoryRegex = /mem\[(\d+)\] = (\d+)/;
+
+  lines.forEach(line => {
+    if (bitMaskRegex.test(line)) {
+      // save the current bitmask, split into character array
+      bitmask = line.match(bitMaskRegex)[1].split(''); // todo: convert to object of which bits to replace
+    } else {
+      // parse both the memory address and the value to be written to that address
+      const matches = line.match(memoryRegex);
+      const address = matches[1];
+      let value = parseInt(matches[2])
+        .toString(2) // convert the value to binary string
+        .padStart(36, '0')
+        .split(''); // split into character array
+
+      bitmask.forEach( (ch, ix) => {
+        if (ch !== 'X') {
+          value[ix] = ch; // apply bitmask
+        }
+      });
+
+      // join the char array into a string, convert to decimal, save to memory
+      memory[address] = parseInt(value.join(''), 2);
+    }
+  })
+
+  // on the assumption that the same memory addresses may be re-written with this program,
+  // loop through the memory addresses and sum the decimal values
+  Object.values(memory).forEach(v => { checksum += v; })
+
+  return checksum;
 }
 
 module.exports = fn;
