79c466aa6842fcb63110529e46c7b5e78fc3b19d
Grace <gracedme.github@gmail.com>
2021-02-08 15:07:10 -0800

    🎄⭐ 2020-15-02: okay now more of that

--- 2020/day15/day.test.js
+++ 2020/day15/day.test.js
@@ -3,11 +3,10 @@ const two = require('./two');
 
 
 describe("day 15", () => {
+  const testInput = `0,3,6`;
   const ACTUAL_INPUT = `18,11,9,0,5,1`;
   describe("part 1", () => {
     test("test input", () => {
-      const testInput = `0,3,6`;
-
       expect(one(testInput, 7)).toBe(1);
       expect(one(testInput, 10)).toBe(0);
       expect(one(testInput, 2020)).toBe(436);
@@ -20,11 +19,16 @@ describe("day 15", () => {
 
   describe("part 2", () => {
     test("test input", () => {
-      // expect(two(testInput)).toBe();
+      expect(two(testInput, 7)).toBe(1);
+      expect(two(testInput, 10)).toBe(0);
+      expect(two(testInput, 2020)).toBe(436);
+      // not sure how to generalize for the cases without 0 as a seed number
+      // expect(two(testInput, `3,1,2`, 2020)).toBe(1836);
+      expect(two(testInput, 30000000)).toBe(175594);
     })
 
     test("actual input", () => {
-      // expect(two(ACTUAL_INPUT)).toBe();
+      expect(two(ACTUAL_INPUT, 30000000)).toBe(116590);
     })
   })
 });
--- 2020/day15/two.js
+++ 2020/day15/two.js
@@ -8,11 +8,30 @@ if (filename) {
   stream.on("data", fn);
 }
 
-function fn(input) {
-  const lines = input.split('\n');
-  let result = 0;
+function fn(input, query) {
+  const seedNumbers = input.split(',').map(n => parseInt(n));
+  const FINAL_TURN = parseInt(query);
 
-  return result;
+  let record = new Map();
+
+  let number = 0;
+  for (let turn = 0; turn < FINAL_TURN; turn++) {
+    if (turn < seedNumbers.length) {
+      number = seedNumbers[turn];
+      record.set(number, turn + 1)
+    } else if (record.has(number)) {
+      const lastSpoken = record.get(number);
+      record.set(number, turn);
+      number = turn - lastSpoken;
+    } else {
+      record.set(number, turn);
+      number = 0;
+    }
+  }
+
+  console.log(number);
+
+  return number;
 }
 
 module.exports = fn;
